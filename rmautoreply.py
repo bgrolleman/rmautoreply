39609d0f3720cd9d9176e70212bdf86f7283fd6b
Greg Trahair <greg.trahair@gmail.com>
2016-06-01 15:42:56 +0200

    Added a unit test to be truly agile.

--- rmautoreply.py
+++ rmautoreply.py
@@ -1,3 +1,16 @@
-#!/usr/bin/python
-print "No"
+#!/usr/bin/env python3
 
+
+def respond_to_communication():
+    '''Be negative to all communications.
+
+    >>> respond_to_communication()
+    'No'
+
+    '''
+    return ('No')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
